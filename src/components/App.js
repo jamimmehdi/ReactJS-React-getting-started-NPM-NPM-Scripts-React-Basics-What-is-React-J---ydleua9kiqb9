062dc2db4cba2e38775e9804cd48620ebaff8521
Jamim Mehdi <jamimmehdi@gmail.com>
2022-07-13 14:23:35 +0530

    assignment-2

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,20 @@ import '../styles/App.css';
 
 const App = () => {
 
-  let [count, setCount] = useState(0)
+  let [count, setCount] = useState(0);
+
+  const doubleClick = () => {
+    alert("cant edit it");
+  }
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  }
 
   return (
     <div class="ball">
-      <h1 class="count" ondoubleclick={() => { alert("cant edit it") }}>{count}</h1>
-      <button class='increment-button' onclick={() => { setCount(count + 1) }}>Increment</button>
+      <h1 class="count" ondoubleclick={doubleClick}>{count}</h1>
+      <button class='increment-button' onclick={incrementCounter}>Increment</button>
     </div>
   )
 }
